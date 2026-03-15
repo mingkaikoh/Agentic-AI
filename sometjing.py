972bf95df3a40fa2b20a61edfb87de1e994abe99
mingkaikoh <mingkaikoh0312@gmail.com>
2026-03-15 14:44:56 +0800

    Update sometjing.py

--- sometjing.py
+++ sometjing.py
@@ -7,5 +7,9 @@ x = 13
 if (x > 3):
     print("x is greater than 3 and x is ", x)
 
-else:
-    print("x is less than or equal to 3 and x is ", x)
+else if (x == 3):
+    print("x is equal to 3 and x is ", x)
+
+else if (x < 3):
+    print("x is less than 3 and x is ", x)
+
