88f9e02697e004d3f21b127eab2efb53cfa3cd91
mingkaikoh <mingkaikoh0312@gmail.com>
2026-03-15 14:56:20 +0800

    Update test1.py

--- test1.py
+++ test1.py
@@ -7,3 +7,7 @@ x = 13
 if x == 9:
     print("x is equal to 9 and x is ", x)
     print("x is less than 9")
+
+elif x == 13:
+    print("x is equal to 13 and x is ", x)
+    print("x is greater than 9")
