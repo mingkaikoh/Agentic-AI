4838518431d3c9bbcc2eb4ca2094190d273d7e4b
mingkaikoh <mingkaikoh0312@gmail.com>
2026-03-23 22:24:58 +0800

    Update grading.py

--- grading.py
+++ grading.py
@@ -28,7 +28,7 @@ def xmum_grading_system():
         if grade == 'Fail':
             print("Status: Required to resit the subjects.")
         else:
-            print("Status: Requirements met.")
+            print("Status: Requirements met. Congrats!")
 
     except ValueError:
         print("Input Error: Marks must be a numerical value.")
